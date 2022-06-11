30f1fc2846e3b8512304cf4c6f085382253c9be6
Ashokaas <antotosun@gmail.com>
2022-06-11 11:43:25 +0200

    Téléchargement de la meilleur video terminé

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # -- Importation des modules --
+from turtle import st
 from pytube import YouTube
 import progressbar as progress
 from colorama import init, Fore
 # -- Importation des fonctions supplémentaires
 from convertions import *
 
-
+global video
 
 def meilleur_qualite():
     qualite_max = 0
@@ -35,7 +36,22 @@ def valider_telechargement(video):
     liste_video = []
     for stream in video:
         if stream.mime_type != "audio/mp4" and stream.mime_type != "audio/webm":
-            liste_video.append({"type": stream.mime_type, "resolution": stream.resolution, "fps": stream.fps, "itag": stream.itag})
+            liste_video.append({"type": stream.mime_type, "resolution": stream.resolution, "fps": (str(stream.fps) + "fps"), "taille": poids_video(stream.filesize), "itag": stream.itag})
+    print(liste_video)
+    for i in range(len(liste_video)):
+        print("Voulez vous télécharger : " +
+                    "\n" + "  ID : " + str(i) + 
+                    "\n" + "    Type :       " + str(liste_video[i]["type"]) + 
+                    "\n" + "    Résolution : " + str(liste_video[i]["resolution"]) + 
+                    "\n" + "    FPS :        " + str(liste_video[i]["fps"]) + 
+                    "\n" + "    Taille :     " + str(liste_video[i]["taille"]))
+    x = int(input("ID de la vidéo à télécharger : "))
+    
+    video = yt.streams.get_by_itag(liste_video[x]["itag"])
+
+    return video
+    
+
 
     
 def telecharger_video(url):
@@ -48,7 +64,7 @@ def telecharger_video(url):
         size = contentsize - bytes_remaining
 
         print('\r' + 'Téléchargement en cours :[%s%s]%.2f%%;' % (
-        '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), size, " / ", contentsize, end='')
+        '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), poids_video(size), "/", poids_video(contentsize), end='')
 
 
     print("Récupération des informations de la vidéo...")
@@ -85,6 +101,8 @@ def telecharger_video(url):
             valider_telechargement(video)
         case 's':
             video = yt.streams
+            for stream in video:
+                print(stream)
         case 'a':
             video = audio()
             #video.download(r'C:\Users\antot\Downloads')
@@ -93,11 +111,13 @@ def telecharger_video(url):
 
 
 
-
-    #video.download()
+    video = video[0]
+    print("Téléchargement en cours...")
+    video.download(r'C:\Users\antot\Desktop\numerisateur-original-universel-biochimique\videos')
 
     print("\nDownload Completed")
 
 
 if __name__ == "__main__":
-    telecharger_video("https://www.youtube.com/watch?v=C-FWMpbBHpU")
+    url = input("Lien de la video : ")
+    telecharger_video(url)
