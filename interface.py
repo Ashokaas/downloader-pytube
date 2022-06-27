ce66f5837727de63e2d33ac8915c6b8d4fa06322
Ashokaas <antotosun@gmail.com>
2022-06-27 21:17:47 +0200

    oui

--- interface.py
+++ interface.py
@@ -1,36 +1,188 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
-from main import informations_video
 from pytube import YouTube
 from urllib.request import urlretrieve
-
+import pytube.exceptions
+import progressbar as progress
+from colorama import init, Fore
+import os
+import subprocess as sp
+from moviepy.config import get_setting
+from subprocess import call
+# -- Importation des fonctions supplémentaires
+from convertions import *
 
 
 # == FONCTIONS ==
-def info_video():
-    
-    dico = informations_video(YouTube(entry_link.get()))
 
-    urlretrieve(dico['url_minia'], 'video/temp/minia_temp/img.jpg')
+
+
+
+# J'écrit "vidéo" sans accent dans les commentaires mais L'ORTHOGRAPHE IL A CHANGE
+
+
+
+
+# -- Clear python console --
+clear = lambda: os.system('cls')
+
+
+
+
+# -- Fusionne l'audio et la video --
+def fusionner_video_audio(video,audio,output, vcodec='copy',
+                             acodec='copy', ffmpeg_output=False,
+                             logger = 'bar'):
+    """ merges video file ``video`` and audio file ``audio`` into one
+        movie file ``output``. """
+    cmd = [get_setting("FFMPEG_BINARY"), "-y", "-i", audio,"-i", video,
+             "-vcodec", vcodec, "-acodec", acodec, output]
+             
+    call(cmd)
+
+
+def supprimer_fichier_dossier(dossier):
+    for fichier in os.listdir(dossier):
+        os.remove(dossier + "/" + fichier)
+
+
+
+    
+
+def audio(yt):
+    # Définie la qualité maximale audio en kbps
+    qualite_max = 0
+    for stream in yt.streams.filter(mime_type="audio/webm"):
+        # S'il est un flux audio
+        if stream.abr != None:
+            # Supprimer le "kbps" pour le garder que le nombre en (int) -- exemple : "128kbs" -> 128
+            qualite_audio = int(stream.abr[0:-4])
+            # Si elle est supérieur à la qulité précédemment enregistré la remplace
+            if qualite_audio > qualite_max:
+                qualite_max = qualite_audio
+    # Récupération du flux audio possédant la meilleure qualité
+    qualite_max = str(qualite_max) + "kbps"
+    video = yt.streams.filter(mime_type="audio/webm", abr=qualite_max)
+    # On retourne le premier élément de la liste car même s'il n'y a qu'un seul meilleur flux audio, le .filter retourne un liste
+    return video[0]
+
+
+
+global itag_to_download
+def choix_video(video):
+    # Création d'une liste de dictionnaires qui stocke les informations importantes à l'utilisateur ainsi qu'un ID pour choisir la video à télécharger
+    liste_video = []
+    # Pour chaque flux "stream" dans video:
+    for stream in video:
+        # S'il est une video/n'est pas un audio
+        if stream.mime_type != "audio/mp4" and stream.mime_type != "audio/webm":
+            # Ajout d'un dico contenant des infos cool qur la video
+            liste_video.append({"type": stream.mime_type, "resolution": stream.resolution, "fps": (str(stream.fps) + "fps"), "taille": poids_video(stream.filesize), "itag": stream.itag})
+    # Tri décroissant en fonction : resolution, fps, taille
+    liste_video.sort(key=lambda element:(int(element['resolution'][0:-1]), int(element['fps'][0:-3]), float(element['taille'][0:-2])), reverse=True)
+    # Affichage des informations pour l'utilisateur sur chaque video et d'un ID pour pouvoir en sélectionner une
+    print("Liste des flux video :")
+    global id
+    id = IntVar()
+    for i in range(len(liste_video)):
+        Label(tab2, text=("ID" + str(i))).grid(column=0, row=i)
+        Label(tab2, text=liste_video[i]["type"]).grid(column=1, row=i)
+        Label(tab2, text=liste_video[i]["resolution"]).grid(column=2, row=i)
+        Label(tab2, text=liste_video[i]["fps"]).grid(column=3, row=i)
+        Label(tab2, text=liste_video[i]["taille"]).grid(column=4, row=i)
+        Radiobutton(tab2, variable=id, value=liste_video[i]["itag"]).grid(column=5, row=i)
+
+    return liste_video, i
+    
+
+
+
+
+
+def telechargement(video):
+
+
+    def progress(streams, chunk: bytes, bytes_remaining: int):
+        contentsize = video.filesize
+        size = contentsize - bytes_remaining
+        print('\r' + 'Téléchargement en cours :[%s%s]%.2f%%;' % (
+        '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), poids_video(size), "/", poids_video(contentsize), end='')
+
+
+    if video.is_progressive == False:
+        audio_for_video = audio(yt)
+        print(audio_for_video)
+
+        file_name = formatage_video_name(audio_for_video.default_filename)
+        video_path = 'video/temp/video_temp'
+        audio_path = 'video/temp/audio_temp'
+
+        print("Téléchargement de l'audio en cours...")
+        audio_for_video.download(output_path=audio_path, filename=file_name)
+
+        print("Téléchargement de la video en cours...")
+        video.download(output_path=video_path, filename=file_name)
+
+        print("Fusion de la video et de l'audio en cours...")
+        fusionner_video_audio((video_path + '/' + file_name), (audio_path + '/' + file_name), ('video/video/' + file_name))
+
+        #supprimer_fichier_dossier('video/temp/video_temp')
+        #supprimer_fichier_dossier('video/temp/audio_temp')
+    else:
+        file_name = formatage_video_name(video.default_filename)
+        video_path = 'video/temp/video_temp'
+        print("Téléchargement de la video en cours...")
+        video.download(output_path=video_path, filename=file_name)
+    
+
+
+
+    
+ # -- Convertir vidéo --
+def convertir_video():
+    
+
+
+    def telecharger():
+        telechargement(yt.streams.get_by_itag(id.get()))
+
+    global yt
+    
+    yt = YouTube(entry_link.get())
+    for stream in yt.streams:
+            print(stream)
+
+    urlretrieve(yt.thumbnail_url, 'video/temp/minia_temp/img.jpg')
     global img
     img = ImageTk.PhotoImage(Image.open('video/temp/minia_temp/img.jpg').resize((4*38, 3*38)))
     miniature["image"] = img
+    label_titre["text"] = "Titre : " + titre_ligne(yt.title)
+    label_chaine["text"] = "Chaîne : " + yt.author
+    label_vues["text"] = "Vues : " + nb_vues(yt.views)
+    label_duree["text"] = "Durée : " + duree(yt.length)
 
-    label_titre["text"] = "Titre : " + dico["titre"]
+    video = yt.streams.filter(mime_type="video/mp4")
+    liste_video, i = choix_video(video)
 
-    label_chaine["text"] = "Chaîne : " + dico["chaine"]
+    entry_id = Entry(tab2, width=3)
+    entry_id.grid(column=0, row=i+1)
+    button_id = Button(tab2, text='Télécharger', command=telecharger)
+    button_id.grid(column=1, row=i+1)
 
-    label_vues["text"] = "Vues : " + dico["vues"]
 
-    label_duree["text"] = "Durée : " + dico["duree"]
+    
 
+
+# == FIN FONCTIONS ==
 
 
 def clear_entry():
     entry_link.delete(0, 'end')
 
-
+def copy(event):
+    clear_entry()
+    entry_link.insert(0, str(entry_link.clipboard_get()))
 
 # -- Configuration de la fenêtre
 root = Tk()
@@ -62,17 +214,21 @@ tabControl.pack(expand = 1, fill ="both", side=LEFT)
 # -- Frame : tab1 -> root
 
 label_link = Label(tab1, text='Lien de la vidéo :', pady=20)
-label_link.pack()
+label_link.grid(column=0, row=0)
 
 entry_link = Entry(tab1, width=50)
-entry_link.pack()
+entry_link.grid(column=0, row=1)
+entry_link.bind("<Button - 3>", copy)
 
-button = Button(tab1, text="❌", command=clear_entry)
-button.pack()
+button_delete = Button(tab1, text="❌", command=clear_entry)
+button_delete.grid(column=1, row=1)
 
-button_link = Button(tab1, text="Convertir", command=info_video)
-button_link.pack()
 
+
+button_link = Button(tab1, text="Convertir", command=convertir_video)
+button_link.grid(column=0, row=2)
+
+tab1.columnconfigure(0, weight=1)
 
 
 # -- Frame : information --
@@ -85,7 +241,7 @@ information.pack(side=RIGHT, fill="both")
 label_info = Label(information, text="Informations de la vidéo :", pady=15)
 label_info.pack()
     # Miniature
-miniature = Label(information, image=None, pady=20)
+miniature = Label(information, image=None)
 miniature.pack()
     # Titre
 label_titre = Label(information, text=None, pady=6)
@@ -99,6 +255,7 @@ label_vues.pack()
     # Duree
 label_duree = Label(information, text=None, pady=6)
 label_duree.pack()
+
 
 
 
